103dd5c75c975dfa56450913c9e9d3079a0a2953
Mariya <mariya.podosinova@gmail.com>
2020-08-20 13:04:35 +0100

    minor style

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import React from "react";
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>App Home</h1>
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <button type="submit">Log In</button>
       </form>
     </div>
   );
